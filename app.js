ab0abea2327f48c351458030ca2749c52a7eeb58
Michael Jackson <mjijackson@gmail.com>
2012-10-09 17:02:03 -0700

    Add next button

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ function makePath() {
   return path.join.apply(path, [__dirname].concat(args));
 }
 
+function leftPad(value, length, padding) {
+  value = String(value);
+  padding = padding || '0';
+
+  while (value.length < length) {
+    value = padding + value;
+  }
+
+  return value;
+}
+
 strata.use(strata.commonLogger);
 strata.use(strata.contentLength);
 strata.use(strata.contentType, 'text/html');
@@ -18,9 +29,20 @@ var layout = fs.readFileSync(makePath('public', 'layout.mustache'), 'utf8');
 
 strata.get('/:number', function (env, callback) {
   var number = env.route.number;
-  var code = fs.readFileSync(makePath('public', number + '.js'), 'utf8');
-  var content = mustache.render(layout, { code: code });
-  callback(200, {}, content);
+  var file = makePath('public', number + '.js');
+
+  fs.exists(file, function (exists) {
+    if (exists) {
+      var code = fs.readFileSync(file, 'utf8');
+      var nextNumber = leftPad(parseInt(number, 10) + 1, 2);
+      var nextFile = makePath('public', nextNumber + '.js');
+      var next = fs.existsSync(nextFile) && JSON.stringify('/' + nextNumber);
+      var content = mustache.render(layout, { code: code, next: next });
+      callback(200, {}, content);
+    } else {
+      strata.utils.notFound(env, callback);
+    }
+  });
 });
 
 strata.get('/', function (env, callback) {
